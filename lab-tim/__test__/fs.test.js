6884f90a1200273337516642a598aa835f07c70c
Tim Turner <timmyturner@me.com>
2017-08-23 14:10:16 -0700

    adds in the read file code

---
+++ lab-tim/__test__/fs.test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const index = require('../index.js')
+
+describe('ModuleName', function() {
+  describe('default properties', () => {
+    test('should have ...' , () => {
+      expect(true).toBeTrue
+    })
+  })
+  describe('#someMethod', () => {
+    test('should have ...' , () => {
+      expect(true).toBeTrue
+    })
+  })
+})
