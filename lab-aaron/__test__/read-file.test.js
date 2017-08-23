162f1d968761690c62e1d68e63b51cbf837eecaa
Aaron McPeek <arn1313@comcast.net>
2017-08-23 15:19:46 -0700

    passing first rounds of test

---
+++ lab-aaron/__test__/read-file.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const readIt = require('../lib/read-file');
+
+describe('Should show first 8 hex values from txt files', function() {
+  test('Should return the first 8 hex characters', done => {
+    console.log(readIt.read());
+    done();
+  });
+});
+
+describe('Should show first 8 hex values from txt files', function() {
+  test('Should return the first 8 hex characters', done => {
+    console.log(readIt.read());
+    done();
+  });
+});
+
+describe('Should show first 8 hex values from txt files', function() {
+  test('Should return the first 8 hex characters', done => {
+    console.log(readIt.read());
+    done();
+  });
+});
