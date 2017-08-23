8329474fd5cbb751fd429655a4dc5bc347761cd4
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-23 14:13:20 -0700

    updating index.js

--- Shaun-Lab03/lib/file-reader.js
+++ Shaun-Lab03/lib/file-reader.js
@@ -1,32 +1,26 @@
 'use-strict';
 
-
 module.exports = exports = {};
 
 const fs = require('fs');
 
 console.log(`current dir: ${__dirname}`);
 
-exports.read = () => {
-  let first, second;
+const fileReader = module.exports = function(paths, callback){
+  let buffers = [];
+  fs.readFile(paths[0], function(err, data){
+    if (err) return callback(err);
+    buffers.push(data.toString('hex', 0, 8));
 
-  fs.readFile(`${__dirname}/../assets/two.txt`, (err, data) => {
-    if(err) console.error(err);
-    console.log('two.txt', data.toString('utf-8', 0, 8));
-    first = data.toString('utf-8', 0, 18);
+    fs.readFile(paths[1], function(err, data){
+      if (err) return callback(err);
+      buffers.push(data.toString('hex', 0, 8));
+
+      fs.readFile(paths[2], function(err, data){
+        if (err) return callback(err);
+        buffers.push(data.toString('hex', 0, 8));
+        return callback(null, buffers);
+      });
+    });
   });
-
-  fs.readFile(`${__dirname}/../assets/one.txt`, (err, data) => {
-    if(err) console.error(err);
-    console.log('one.txt', data.toString('utf-8', 0, 8));
-    second = data.toString('utf-8', 0, 18);
-  });
-
-  fs.readFile(`${__dirname}/../assets/three.txt`, (err, data) => {
-    if(err) console.error(err);
-    console.log('three.txt', data.toString('utf-8', 0, 8));
-    second = data.toString('utf-8', 0, 18);
-  });
-
-  return {first, second};
 };
