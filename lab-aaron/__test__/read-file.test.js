67124068d69ed821923eff3f39c53c5145a1e217
Aaron McPeek <arn1313@comcast.net>
2017-08-23 16:47:04 -0700

    updating

--- lab-aaron/__test__/read-file.test.js
+++ lab-aaron/__test__/read-file.test.js
@@ -1,24 +1,32 @@
 'use strict';
 
 const readIt = require('../lib/read-file');
+const fs = require('fs');
 
 describe('Should show first 8 hex values from txt files', function() {
   test('Should return the first 8 hex characters', done => {
-    console.log(readIt.read());
-    done();
-  });
-});
+    let firsty, secondy, thirdy;
 
-describe('Should show first 8 hex values from txt files', function() {
-  test('Should return the first 8 hex characters', done => {
-    console.log(readIt.read());
-    done();
-  });
-});
+    fs.readFile(`${__dirname}/../assets/one.txt`, (err, data) => {
+      if (err) console.error(err);
+      firsty = data.toString('hex', 0, 8);
+    });
 
-describe('Should show first 8 hex values from txt files', function() {
-  test('Should return the first 8 hex characters', done => {
-    console.log(readIt.read());
-    done();
+    fs.readFile(`${__dirname}/../assets/two.txt`, (err, data) => {
+      secondy = data.toString('hex', 0, 8);
+    });
+
+    fs.readFile(`${__dirname}/../assets/three.txt`, (err, data) => {
+      thirdy = data.toString('hex', 0, 8);
+    });
+    readIt.read( (data) => {
+      let odata = [data.first, data.second, data.third];
+      let ndata = [firsty, secondy, thirdy];
+      // console.log(data.first, data.second, data.third);
+      // console.log(firsty, secondy, thirdy);
+      // expect(data.first, data.second, data.third).toEqual(firsty, secondy, thirdy);
+
+      done();
+    });
   });
 });
