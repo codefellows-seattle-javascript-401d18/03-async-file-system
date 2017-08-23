f2ebe135b3eb6f696e14bfd8a58af13ace0d336f
GavinThomas1192 <gthomas1192@gmail.com>
2017-08-23 16:11:29 -0700

    I think I got it

--- lab-gavin/lib/read.js
+++ lab-gavin/lib/read.js
@@ -1,32 +1,37 @@
 'use-strict';
 
 
-module.exports = exports = {};
+const yolo = module.exports = {};
 
 const fs = require('fs');
 
-console.log(`current dir: ${__dirname}`);
 
-exports.read = () => {
-  let first, second;
 
-  fs.readFile(`${__dirname}/../assets/two.txt`, (err, data) => {
-    if(err) console.error(err);
-    console.log('two.txt', data.toString('utf-8', 0, 8));
-    first = data.toString('utf-8', 0, 18);
-  });
+yolo.read = (callback) => {
+  let first, second, third;
 
   fs.readFile(`${__dirname}/../assets/one.txt`, (err, data) => {
     if(err) console.error(err);
-    console.log('one.txt', data.toString('utf-8', 0, 8));
-    second = data.toString('utf-8', 0, 18);
+    first = data.toString('utf-8', 0, 8);
+
+
+    fs.readFile(`${__dirname}/../assets/two.txt`, (err, data) => {
+      if(err) console.error(err);
+      second = data.toString('utf-8', 0, 8);
+
+
+      fs.readFile(`${__dirname}/../assets/three.txt`, (err, data) => {
+        if(err) console.error(err);
+        third = data.toString('utf-8', 0, 8);
+
+
+
+        callback({first, second, third});
+
+      });
+    });
   });
 
-  fs.readFile(`${__dirname}/../assets/three.txt`, (err, data) => {
-    if(err) console.error(err);
-    console.log('three.txt', data.toString('utf-8', 0, 8));
-    second = data.toString('utf-8', 0, 18);
-  });
-
-  return {first, second};
 };
+
+// yolo.read();
