a95297e4e865019b77c56e95b6ab74a7696bec68
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-23 13:30:39 -0700

    issues with node modules

---
+++ Shaun-Lab03/__test__/file-reader-test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const readFile = require('../lib/file-reader');
+
+
+describe('File Reader Module', () => {
+  test('with improper file path', () => {
+    it('should return and error', done => {
+      fileReader(`${__dirname}/not-a-file.txt`, err => {
+        expect(err).toEqual('error');
+        done();
+      });
+    });
+  });
+//     console.log(readFile.read());
+//     done();
+//   });
+//
+//   test('should return some data', done => {
+//     console.log(readFile.read());
+//     done();
+//   });
+//
+//   test('should return some data', done => {
+//     console.log(readFile.read());
+//     done();
+//   });
+// });
