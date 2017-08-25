0b867afd3c3c917a122a9f5d2a32ce535851196e
lonewalker72 <walkisai.72@gmail.com>
2017-08-24 22:17:36 -0700

    added console.log back in for tech req

--- lab-isaiah/lib/read-file.js
+++ lab-isaiah/lib/read-file.js
@@ -11,14 +11,17 @@ txtOrder.read = (callback) => {
   fs.readFile(`${__dirname}/../assests/one.txt`, (err, data) => {
     if(err) console.error(err);
     first = data.toString('hex', 0, 8);
+    console.log('first:', first);
 
     fs.readFile(`${__dirname}/../assests/two.txt`, (err, data) => {
       if(err) console.error(err);
       second = data.toString('hex', 0, 8);
+      console.log('second:', second);
 
       fs.readFile(`${__dirname}/../assests/three.txt`, (err, data) => {
         if(err) console.error(err);
         third = data.toString('hex', 0, 8);
+        console.log('third:', third);
         callback({first, second, third});
       });
     });
